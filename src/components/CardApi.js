c6673d6f96b7a21a14a3faab26a9d6dc2e04f492
farhodrw <farhodrailway@gmail.com>
2022-03-31 19:52:44 +0500

    added loader

--- src/components/CardApi.js
+++ src/components/CardApi.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import axios from 'axios'
 import { useState, useEffect } from 'react';
+import { css } from "@emotion/react";
+import ClipLoader from "react-spinners/ClipLoader";
 
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: #F1D00A;
+`;
 
 export default function BasicCard() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false)
+  let [color, setColor] = useState("yellow");
+
 
   const retrieveApis = async () => {
     const response = await axios.get('http://localhost:3002/data')
@@ -15,8 +26,17 @@ export default function BasicCard() {
 
   useEffect(() => {
     const getAllApis = async () => {
-      const allApis = await retrieveApis()
-      if (allApis) setData(allApis)
+      try {
+        setLoading(true)
+        const allApis = await retrieveApis()
+        if (allApis) setData(allApis)
+      } catch (error) {
+        console.log(error)
+      }
+      finally {
+        setLoading(false)
+      }
+
     }
     getAllApis()
   }, [])
@@ -31,48 +51,52 @@ export default function BasicCard() {
           width: "30%",
           height: "35px",
           border: "4px solid #F1D00A",
-
-
-
         }}
         onChange={event => { setSearchTerm(event.target.value) }}>
-
       </input>
 
 
-      <div style={{
-        display: "flex",
-        flexWrap: "wrap"
-      }}>
-        {data.filter((val) => {
-          if (searchTerm === " ") { return val }
-          else if (val.API.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return val
-          }
-        }).map((val, key) => {
-          return (
-
-            <div key={key}
-              style={
-                {
-                  backgroundColor: "#132C33",
-                  color: "#FFF",
-
-                  border: "1px solid blue",
-                  width: "30%",
-
-                  margin: "9px",
-                  padding: "7px"
-                }
-              }>
-              <h3>{val.API}</h3>
-              <h5>{val.Category}</h5>
-              <p>{val.Description}</p>
-              <a href={val.Link} style={{ color: "#F1D00A" }}>Visit here for more</a>
-            </div>
-          )
-        })}
+      {loading ? <div className="sweet-loading">
+        <ClipLoader color={color} loading={loading} css={override} size={150} />
+        <h1>Loading, please wait...</h1>
       </div>
+        :
+        <div style={{
+          display: "flex",
+          flexWrap: "wrap"
+        }}>
+
+          {data.filter((val) => {
+            if (searchTerm === " ") { return val }
+            else if (val.API.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val
+            }
+          }).map((val, key) => {
+            return (
+
+              <div key={key}
+                style={
+                  {
+                    backgroundColor: "#132C33",
+                    color: "#FFF",
+
+                    border: "1px solid blue",
+                    width: "30%",
+
+                    margin: "9px",
+                    padding: "7px"
+                  }
+                }>
+                <h3>{val.API}</h3>
+                <h5>{val.Category}</h5>
+                <p>{val.Description}</p>
+                <a href={val.Link} style={{ color: "#F1D00A" }}>Visit here for more</a>
+              </div>
+            )
+          })}
+        </div>}
+
+
     </div >
 
   );
